b737e9d76e0d20cfe7df1666b7d86b1ce292495b
Ito <ckdwo23@gmail.com>
2025-06-09 16:36:47 +0900

    chore: Word 모델에 샘플 데이터 추가

--- MunKanji/Word.swift
+++ MunKanji/Word.swift
@@ -21,6 +21,13 @@ struct Word: Codable {
         case firstWord = "word1"
         case secondWord = "word2"
     }
+    
+    
+    static let samples: [Word] = [
+            Word(grade: "1", firstWord: "차표", secondWord: "주차", kanji: "車", korean: "수레 차", sound: "しゃ", meaning: "탈것, 자동차"),
+            Word(grade: "1", firstWord: "화산", secondWord: "불꽃", kanji: "火", korean: "불 화", sound: "か", meaning: "불"),
+            Word(grade: "1", firstWord: "수영", secondWord: "수도", kanji: "水", korean: "물 수", sound: "すい", meaning: "물")
+        ]
 }
 
 
