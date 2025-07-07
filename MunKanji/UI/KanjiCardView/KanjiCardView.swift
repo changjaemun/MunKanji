4e7ee25df8220acd9cbe157807c9a582ca237c33
Ito <ckdwo23@gmail.com>
2025-07-07 18:01:55 +0900

    fix: 예시 단어 사라져서 KanjiCardView UI 수정

--- MunKanji/UI/KanjiCardView/KanjiCardView.swift
+++ MunKanji/UI/KanjiCardView/KanjiCardView.swift
@@ -1,25 +1,16 @@
-//
-//  KanjiCardView.swift
-//  MunKanji
-//
-//  Created by 문창재 on 6/4/25.
-//
-
 import SwiftUI
 
 struct KanjiCardView: View {
-//    let word:Word = Word(firstWord: "電車[でんしゃ] 전철", secondWord: "車[くるま] 차, 자동차", kanji: "車",korean: "수레 차", sound: "しゃ", meaning: "くるま")
-    
     let kanji: Kanji
     
     var body: some View {
                 ZStack{
                     Rectangle()
                         .foregroundStyle(.white)
-                      .frame(width: 338, height: 521)
+                      .frame(width: 338, height: 400)
                       .cornerRadius(20)
                       .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
-                    VStack(alignment:.leading){
+                    VStack{
                         VStack{
                             Text(kanji.kanji)
                                 .foregroundStyle(.main)
@@ -30,34 +21,28 @@ struct KanjiCardView: View {
                         }.frame(width: 338)
                             .padding(.bottom)
                         Divider()
-                            .frame(width: 330)
-                        VStack(alignment:.leading){
-                            Text("음: \(kanji.sound)")
-                                .foregroundStyle(.main)
-                                .font(.pretendardRegular(size: 24))
-                                .padding(10)
-                            Text("훈: \(kanji.meaning)")
-                                .foregroundStyle(.main)
-                                .font(.pretendardRegular(size: 24))
-                                .padding(10)
-                        }.padding()
+                            .frame(width: 310)
+                        HStack{
+                            VStack(alignment:.leading){
+                                Text("음: \(kanji.sound)")
+                                    .foregroundStyle(.main)
+                                    .font(.pretendardRegular(size: 24))
+                                    .padding(10)
+                                Text("훈: \(kanji.meaning)")
+                                    .foregroundStyle(.main)
+                                    .font(.pretendardRegular(size: 24))
+                                    .padding(10)
+                            }.padding()
+                            Spacer()
+                        }
                         
-                        
-                        Divider()
-                            .frame(width: 330)
-                        VStack(alignment:.leading){
-                            Text(kanji.firstWord ?? "")
-                                .foregroundStyle(.main)
-                                .font(.pretendardRegular(size: 20))
-                                .padding(10)
-                            Text(kanji.secondWord ?? "")
-                                .foregroundStyle(.main)
-                                .font(.pretendardRegular(size: 20))
-                                .padding(10)
-                        }.padding()
-                        
-                    }.frame(width: 338, height: 521)
+                    }.frame(width: 338, height: 400)
                     
                 }
         }
     }
+
+#Preview {
+    let kanji = Kanji(id: 0, grade: "소학교 1학년", kanji: "車", korean: "수레 거, 수레 차", sound: "しゃ", meaning: "くるま")
+    KanjiCardView(kanji: kanji)
+}
