051c6d62d0044ac1c522e17ac62999a50e811fce
Ito <ckdwo23@gmail.com>
2025-08-08 15:47:31 +0900

    feat: 칸지카드뷰 Status별 상태 색 나타내는 띠 추가

--- MunKanji/UI/SubView/KanjiCardView/KanjiCardView.swift
+++ MunKanji/UI/SubView/KanjiCardView/KanjiCardView.swift
@@ -11,6 +11,18 @@ struct KanjiCardView: View {
             return formatter
         }()
     
+    func statusBarColor() -> Color {
+        if studyLog.status == .incorrect {
+            return .incorrect
+        }
+        
+        if let nextReviewDate = studyLog.nextReviewDate, nextReviewDate <= Date() {
+            return .point
+        }
+        
+        return .white
+    }
+    
     var body: some View {
                 ZStack{
                     Rectangle()
@@ -18,6 +30,13 @@ struct KanjiCardView: View {
                       .frame(width: 338, height: 360)
                       .cornerRadius(20)
                       .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+                    VStack{
+                        Rectangle()
+                            .foregroundStyle(statusBarColor()) // 리뷰, 새로운 한자, 틀린 한자 인지에 따라 색 변경
+                            .frame(width: 338, height: 24)
+                            .cornerRadius(20, corners: [.topLeft, .topRight])
+                        Spacer()
+                    }.frame(height: 360)
                     VStack{
                         VStack{
                             Text(kanji.kanji)
@@ -27,7 +46,7 @@ struct KanjiCardView: View {
                                 .foregroundStyle(.main)
                                 .font(.pretendardRegular(size: 24))
                         }.frame(width: 338)
-                            .padding(.bottom)
+                            .padding(.bottom, 30)
                         Divider()
                             .frame(width: 310)
                         HStack{
@@ -38,21 +57,7 @@ struct KanjiCardView: View {
                                 Text("훈: \(kanji.meaning)")
                                     .foregroundStyle(.main)
                                     .font(.pretendardRegular(size: 24))
-                                if let lastStudiedDate = studyLog.lastStudiedDate{
-                                    Text("맞힌 날: \(dateFormatter.string(from: lastStudiedDate))")
-                                        .foregroundStyle(.main)
-                                        .font(.pretendardRegular(size: 16))
-                                }else{
-                                    Text("맞힌 날: 모름")
-                                        .foregroundStyle(.main)
-                                        .font(.pretendardRegular(size: 16))
-                                }
-                                if let nextReviewDate = studyLog.nextReviewDate{
-                                    Text("다음 리뷰 날짜: \(dateFormatter.string(from: nextReviewDate))")
-                                        .foregroundStyle(.main)
-                                        .font(.pretendardRegular(size: 16))
-                                }
-                            }.padding()
+                            }.padding(.horizontal)
                             Spacer()
                         }.padding()
                         
@@ -62,7 +67,26 @@ struct KanjiCardView: View {
         }
     }
 
-//#Preview {
-//    let kanji = Kanji(id: 0, grade: "소학교 1학년", kanji: "車", korean: "수레 거, 수레 차", sound: "しゃ", meaning: "くるま")
-//    KanjiCardView(kanji: kanji)
-//}
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+#Preview {
+    let kanji = Kanji(id: 0, grade: "소학교 1학년", kanji: "車", korean: "수레 거, 수레 차", sound: "しゃ", meaning: "くるま")
+    KanjiCardView(kanji: kanji, studyLog: StudyLog(kanjiID: 0))
+        .background(.black)
+}
